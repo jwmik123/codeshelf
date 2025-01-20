9385a09072ba146b2aa4f5e5f8120209d82c1cae
Joël Mik <contact@joelmik.nl>
2025-01-20 18:03:20 +0100

    likes added to type in snippet form

--- app/components/SnippetForm.tsx
+++ app/components/SnippetForm.tsx
@@ -94,6 +94,7 @@ function SnippetForm({ theme }: { theme: "dark" | "light" }) {
         code: code || "",
         language: language || "javascript",
         shelf: shelf || "default",
+        likes: 0,
         created_at: new Date().toISOString(),
         updated_at: new Date().toISOString(),
         user_id: null,
